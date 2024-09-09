520f309095ba4ab803b55be5c743d8dca2130e68
Hunter Wright <hgw260@email.vccs.edu>
2024-09-09 22:46:55 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Project-1/main.cpp
@@ -0,0 +1,13 @@
+// C++ Basics Programming Project
+//Project 1. Distance per Tank of Gas
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int tank = 20;
+    float mpg = 23.5;
+    float town = mpg * tank;
+
+        cout << "The distance the car can travel in town is: \n" << town << " miles." << endl;
+}
