53f5dbc26edfde8e4d5ce80eb6066b2457c8396f
Hunter Wright <hgw260@email.vccs.edu>
2024-09-09 22:56:13 +0000

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 using namespace std;
 
+//The function main where everything happens
 int main ()
 {
+  //Establishes the variables for the program
     int tank = 20;
     float mpg = 23.5;
     float town = mpg * tank;
-
+  //This prints the results of the equation
         cout << "The distance the car can travel in town is: \n" << town << " miles." << endl;
 }
+
+// By Hunter Wright
