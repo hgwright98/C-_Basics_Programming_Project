6e81cca10e3498b70a28cd93bde032c95cc3e284
Hunter Wright <hgw260@email.vccs.edu>
2024-09-09 23:22:42 +0000

    Started Project 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // C++ Basics Programming Project
-//Project 1. Distance per Tank of Gas
+//Project 2. Energy Drink Consumption
 #include <iostream>
 using namespace std;
 
@@ -7,11 +7,15 @@ using namespace std;
 int main ()
 {
   //Establishes the variables for the program
-    int tank = 20;
-    float mpg = 23.5;
-    float town = mpg * tank;
+    int customers = 16500;
+    float percentEnergy = 0.15;
+    float percentCitrus = 0.58;
+    float energyDrink = customers * percentEnergy;
+    int citrusDrink = energyDrink * percentCitrus;
   //This prints the results of the equation
-        cout << "The distance the car can travel in town is: \n" << town << " miles." << endl;
+        cout << "The number of customers in the survey that purchase one or more energy drinks: " << energyDrink <<endl;
+  //Note I debated on trying to create a function to round this number up but found that it would not be all that helpful as it wouldnt impact the data in a meaningful way
+        cout << "The number of customers out of those who that purchase one or more energy drinks that prefer citrus-flavored energy drinks: " << citrusDrink <<endl;
 }
 
 // By Hunter Wright
